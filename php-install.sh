56b9933632a222c9ab94e5007cf6f6c8ec1259e1
Eirik S. Morland <eirik@morland.no>
2020-06-27 20:09:24 +0200

    Make this better for arm

--- php-install.sh
+++ php-install.sh
@@ -1,8 +1,20 @@
 set -eu
 
+apk add --no-cache --virtual .dd-build-deps libpng-dev libjpeg-turbo-dev postgresql-dev libxml2-dev $PHPIZE_DEPS \
+  && docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr \
+  && docker-php-ext-install gd mbstring pdo_mysql pdo_pgsql zip \
+  && docker-php-ext-install opcache bcmath soap \
+  && pecl install redis-3.1.1 \
+  && docker-php-ext-enable redis \
+  && apk add --no-cache sudo git libpng libjpeg libpq libxml2 mysql-client openssh-client rsync patch bash imagemagick \
+    imagemagick-libs imagemagick-dev autoconf bzip2-dev g++ make icu-dev \
+  && apk del .dd-build-deps
+
+curl -sS https://getcomposer.org/installer | php \
+  && mv composer.phar /usr/local/bin/composer
+
 composer self-update \
     && composer global require hirak/prestissimo \
-    && apk add --no-cache imagemagick imagemagick-libs imagemagick-dev autoconf bzip2-dev g++ make icu-dev \
     && yes | pecl install apcu mongodb imagick \
     && docker-php-ext-configure intl --enable-intl \
     && docker-php-ext-install exif bz2 pcntl intl \
