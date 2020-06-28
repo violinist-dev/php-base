0af69eaa43381ad057463370e5be269638d9c48a
Eirik S. Morland <eirik@morland.no>
2020-06-28 13:45:54 +0200

    Make sure we have cloned SAs

--- php-install.sh
+++ php-install.sh
@@ -24,14 +24,15 @@ curl -sS https://getcomposer.org/installer | php \
 
 composer self-update \
     && composer global require hirak/prestissimo \
-    && mkdir ~/.ssh/ \
-    && docker-php-ext-install redis exif gd \
-    && ssh-keyscan -t rsa,dsa git.drupal.org >> ~/.ssh/known_hosts \
+    && mkdir ~/.ssh/
+
+ssh-keyscan -t rsa,dsa git.drupal.org >> ~/.ssh/known_hosts \
     && ssh-keyscan -t rsa,dsa gitlab.com >> ~/.ssh/known_hosts \
     && ssh-keyscan -t rsa,dsa bitbucket.org >> ~/.ssh/known_hosts \
-    && ssh-keyscan -t rsa,dsa github.com >> ~/.ssh/known_hosts \
-    && git clone https://github.com/FriendsOfPHP/security-advisories /root/.symfony/cache/security-advisories \
-    && git clone https://github.com/violinist-dev/drupal-contrib-sa /root/drupal-contrib-sa
+    && ssh-keyscan -t rsa,dsa github.com >> ~/.ssh/known_hosts
+
+git clone https://github.com/FriendsOfPHP/security-advisories /root/.symfony/cache/security-advisories
+git clone https://github.com/violinist-dev/drupal-contrib-sa /root/drupal-contrib-sa
 
 machine=`uname -m 2>/dev/null || /usr/bin/uname -m`
 case ${machine} in
