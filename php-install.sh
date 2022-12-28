ab79bc19d75a3f625d72ef2b53d4ee65c15f7b00
Eirik Stanghelle Morland <eirik@morland.no>
2022-12-28 13:46:53 +0100

    Update php-install.sh

--- php-install.sh
+++ php-install.sh
@@ -18,8 +18,15 @@ else
 fi
 
 
-yes | pecl install mailparse apcu igbinary oauth imagick rdkafka
+yes | pecl install apcu igbinary oauth imagick rdkafka
 echo "" | pecl install memcached
+
+if [ $PHP_VERSION = "7.2" ]
+then
+    yes | pecl install mailparse-3.1.3
+else
+    yes | pecl mailparse
+fi
 
 if [ $PHP_VERSION = "8.0" ] || [ $PHP_VERSION = "8.1" ] || [ $PHP_VERSION = "8.2" ]
 then
