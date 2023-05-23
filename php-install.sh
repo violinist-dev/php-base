b44054a2c94f0cc91802e08c64bdbc316c8a505e
Eirik S. Morland <eirik@morland.no>
2023-05-23 20:32:33 +0200

    Fix syntax

--- php-install.sh
+++ php-install.sh
@@ -31,7 +31,7 @@ fi
 if [ $PHP_VERSION = "7.2" ]
 then
     yes | pecl install sqlsrv-5.8.1
-elif [ $PHP_VERSION = "7.3"]
+elif [ $PHP_VERSION = "7.3" ]
 then
     yes | pecl install sqlsrv-5.10.1
 else
