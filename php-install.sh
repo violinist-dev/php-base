76f3ac7ed56a7eabbc42fd2ebb5c122cd991e209
Eirik Stanghelle Morland <eirik@morland.no>
2023-05-23 21:44:16 +0200

    Also pdo_sqlsrv (#72)

--- php-install.sh
+++ php-install.sh
@@ -43,12 +43,12 @@ fi
 
 if [ $PHP_VERSION = "7.2" ]
 then
-    yes | pecl install sqlsrv-5.8.1
+    yes | pecl install sqlsrv-5.8.1 pdo_sqlsrv-5.8.1
 elif [ $PHP_VERSION = "7.3" ] || [ $PHP_VERSION = "7.4" ]
 then
-    yes | pecl install sqlsrv-5.10.1
+    yes | pecl install sqlsrv-5.10.1 pdo_sqlsrv-5.10.1
 else
-    yes | pecl install sqlsrv
+    yes | pecl install sqlsrv pdo_sqlsrv
 fi
 
 if [ $PHP_VERSION = "8.0" ] || [ $PHP_VERSION = "8.1" ] || [ $PHP_VERSION = "8.2" ]
@@ -60,7 +60,7 @@ fi
 
 docker-php-ext-configure intl
 docker-php-ext-install intl
-docker-php-ext-enable intl yaml sqlsrv decimal
+docker-php-ext-enable intl yaml sqlsrv pdo_sqlsrv decimal
 if [ $PHP_VERSION = "7.4" ] || [ $PHP_VERSION = "8.0" ] || [ $PHP_VERSION = "8.1" ] || [ $PHP_VERSION = "8.2" ]
 then
     apk add --no-cache oniguruma-dev
