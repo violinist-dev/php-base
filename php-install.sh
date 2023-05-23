bdb676478551b3bc8e6bc3a85608558b36b238ef
Eirik Stanghelle Morland <eirik@morland.no>
2023-05-23 20:51:40 +0200

    ext sqlsrv (#70)

--- php-install.sh
+++ php-install.sh
@@ -1,6 +1,6 @@
 set -eu
 
-apk add --no-cache yaml-dev ldb-dev libldap openldap-dev pcre-dev libxslt-dev imap-dev sudo git libpng libjpeg libpq libxml2 mysql-client openssh-client rsync patch bash imagemagick libzip-dev \
+apk add --no-cache unixodbc-dev yaml-dev ldb-dev libldap openldap-dev pcre-dev libxslt-dev imap-dev sudo git libpng libjpeg libpq libxml2 mysql-client openssh-client rsync patch bash imagemagick libzip-dev \
     imagemagick-libs imagemagick-dev librdkafka-dev autoconf g++ make icu-dev libpng-dev libjpeg-turbo-dev postgresql-dev libxml2-dev bzip2-dev icu icu-dev libmemcached-dev $PHPIZE_DEPS
 
 pecl channel-update pecl.php.net
@@ -28,6 +28,16 @@ else
     yes | pecl install mailparse
 fi
 
+if [ $PHP_VERSION = "7.2" ]
+then
+    yes | pecl install sqlsrv-5.8.1
+elif [ $PHP_VERSION = "7.3" ] || [ $PHP_VERSION = "7.4" ]
+then
+    yes | pecl install sqlsrv-5.10.1
+else
+    yes | pecl install sqlsrv
+fi
+
 if [ $PHP_VERSION = "8.0" ] || [ $PHP_VERSION = "8.1" ] || [ $PHP_VERSION = "8.2" ]
 then
     mkdir -p /usr/src/php/ext/redis && curl -fsSL https://pecl.php.net/get/redis | tar xvz -C "/usr/src/php/ext/redis" --strip 1 && docker-php-ext-install redis
@@ -37,7 +47,7 @@ fi
 
 docker-php-ext-configure intl
 docker-php-ext-install intl
-docker-php-ext-enable intl yaml
+docker-php-ext-enable intl yaml sqlsrv
 if [ $PHP_VERSION = "7.4" ] || [ $PHP_VERSION = "8.0" ] || [ $PHP_VERSION = "8.1" ] || [ $PHP_VERSION = "8.2" ]
 then
     apk add --no-cache oniguruma-dev
