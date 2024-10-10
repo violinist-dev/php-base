6b94f1f1156d10cc0825789b4d577d2e18126ff8
Eirik Stanghelle Morland <eirik@morland.no>
2024-10-10 10:47:24 +0200

    Revert "Remove everything related to Symfony cli and SA ymls (#127)"
    This reverts commit c05e455bff176df651afcc497589c2a18783532d.

--- php-install.sh
+++ php-install.sh
@@ -174,6 +174,33 @@ ssh-keyscan -t rsa,dsa gitlab.com >> ~/.ssh/known_hosts
 ssh-keyscan -t rsa,dsa bitbucket.org >> ~/.ssh/known_hosts
 ssh-keyscan -t rsa,dsa github.com >> ~/.ssh/known_hosts
 
+git clone https://github.com/FriendsOfPHP/security-advisories /root/.symfony/cache/security-advisories
+git clone https://github.com/violinist-dev/drupal-contrib-sa /root/drupal-contrib-sa
+
+machine=`uname -m 2>/dev/null || /usr/bin/uname -m`
+case ${machine} in
+    arm|armv7*)
+        machine="arm"
+        ;;
+    aarch64*|armv8*)
+        machine="arm64"
+        ;;
+    i386)
+        machine="386"
+        ;;
+    x86_64)
+        machine="amd64"
+        ;;
+    *)
+        output "  [ ] You architecture (${machine}) is not currently supported" "error"
+        exit 1
+        ;;
+esac
+
+wget https://github.com/symfony/cli/releases/download/v4.16.3/symfony_linux_${machine}.gz -O /tmp/symfony.gz
+gzip -d /tmp/symfony.gz
+chmod 755 /tmp/symfony
+mv /tmp/symfony /usr/local/bin/symfony
 wget https://getcomposer.org/download/latest-2.2.x/composer.phar -O /tmp/composer22
 chmod 755 /tmp/composer22
 mv /tmp/composer22 /usr/local/bin/composer22
