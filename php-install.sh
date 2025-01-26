7b90e98bb38334724bf07a0ca616ddd026ebbf5d
Eirik Stanghelle Morland <eirik@morland.no>
2025-01-26 18:54:36 +0100

    Install pre compiled mongo

--- php-install.sh
+++ php-install.sh
@@ -1,6 +1,8 @@
 set -eu
 
-apk add --no-cache unixodbc-dev gmp-dev yaml-dev ldb-dev libldap openldap-dev pcre-dev libxslt-dev imap-dev sudo git libpng libjpeg libpq libxml2 mysql-client openssh-client rsync patch bash imagemagick libzip-dev \
+PHP_VERSION_NO_DOT="${PHP_VERSION/8.1/81}"
+
+apk add --no-cache php$PHP_VERSION_NO_DOT-mongodb unixodbc-dev gmp-dev yaml-dev ldb-dev libldap openldap-dev pcre-dev libxslt-dev imap-dev sudo git libpng libjpeg libpq libxml2 mysql-client openssh-client rsync patch bash imagemagick libzip-dev \
     imagemagick-libs gettext gettext-dev imagemagick-dev librdkafka-dev autoconf g++ make icu-dev libpng-dev libjpeg-turbo-dev postgresql-dev libxml2-dev bzip2-dev icu icu-dev libmemcached-dev linux-headers $PHPIZE_DEPS
 
 case $PHP_VERSION in
