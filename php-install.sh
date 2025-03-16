96daab818079b012ece9ccd93ded29935a122353
Eirik Stanghelle Morland <eirik@morland.no>
2025-03-16 07:11:31 +0100

    Avoid building imagick from source

--- php-install.sh
+++ php-install.sh
@@ -130,18 +130,6 @@ case $PHP_VERSION in
     && echo "extension=imagick.so" > /usr/local/etc/php/conf.d/ext-imagick.ini \
     && rm -rf /tmp/imagick*
     ;;
-  8.3) 
-    curl -fL -o imagick.tgz 'https://pecl.php.net/get/imagick-3.7.0.tgz'; \
-	echo '5a364354109029d224bcbb2e82e15b248be9b641227f45e63425c06531792d3e *imagick.tgz' | sha256sum -c -; \
-	tar --extract --directory /tmp --file imagick.tgz imagick-3.7.0; \
-	grep '^//#endif$' /tmp/imagick-3.7.0/Imagick.stub.php; \
-	test "$(grep -c '^//#endif$' /tmp/imagick-3.7.0/Imagick.stub.php)" = '1'; \
-	sed -i -e 's!^//#endif$!#endif!' /tmp/imagick-3.7.0/Imagick.stub.php; \
-	grep '^//#endif$' /tmp/imagick-3.7.0/Imagick.stub.php && exit 1 || :; \
-	docker-php-ext-install /tmp/imagick-3.7.0; \
-	rm -rf imagick.tgz /tmp/imagick-3.7.0; \
-    docker-php-ext-enable imagick
-    ;;
   *)     
     yes | pecl install imagick
     docker-php-ext-enable imagick
